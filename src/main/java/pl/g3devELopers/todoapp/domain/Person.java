c8aae8522ca47c890383825fafcc57b61768da79
Geryk1988 <72203219+Geryk1988@users.noreply.github.com>
2026-01-17 18:13:00 +0100

    Delete src directory

--- src/main/java/pl/g3devELopers/todoapp/domain/Person.java
+++
@@ -1,5 +0,0 @@
-package pl.g3devELopers.todoapp.domain;
-
-public record Person(String name, String surname, String email) {
-
-}
--- src/main/java/pl/g3devELopers/todoapp/dto/CreatePerson.java
+++
@@ -1,4 +0,0 @@
-package pl.g3devELopers.todoapp.dto;
-
-public record CreatePerson(String name, String surname, String email) {
-}
