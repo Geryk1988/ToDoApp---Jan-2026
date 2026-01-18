22f4db526b795dc96f2935f310ba7a54daef56b9
Geryk1988 <72203219+Geryk1988@users.noreply.github.com>
2026-01-18 17:34:03 +0100

    Delete src directory

--- src/main/java/pl/g3devELopers/todoapp/dto/person/CreatePerson.java
+++
@@ -1,4 +0,0 @@
-package pl.g3devELopers.todoapp.dto.person;
-
-public record CreatePerson(String name, String surname, String email) {
-}
--- src/main/java/pl/g3devELopers/todoapp/dto/task/TaskDto.java
+++
@@ -1,4 +0,0 @@
-package pl.g3devELopers.todoapp.dto.task;
-
-public record TaskDto(Long id, String taskName, String taskDesc) {
-}
